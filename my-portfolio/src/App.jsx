d4940ead6a0b20f6a1da74d072f15498202fa823
KingKimSilverWolf <129137737+KingKimSilverWolf@users.noreply.github.com>
2025-04-07 22:28:36 -0400

    Created the project and the components that will be used in the portfolio.

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import { NavbarDemo } from "./components/NavbarDemo";
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarDemo />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ my-portfolio/src/components/NavbarDemo.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Menu, MenuItem, HoveredLink } from "./ui/navbar-menu";
+import { cn } from "../lib/utils";
+
+export function NavbarDemo() {
+  const [active, setActive] = useState(null);
+
+  return (
+    <div className="relative w-full flex items-center justify-center">
+      <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50")}>
+        <Menu setActive={setActive}>
+          {/* Home */}
+          <MenuItem setActive={setActive} active={active} item="Home">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="#home">Home</HoveredLink>
+            </div>
+          </MenuItem>
+          {/* About */}
+          <MenuItem setActive={setActive} active={active} item="About">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="#about">About Me</HoveredLink>
+            </div>
+          </MenuItem>
+          {/* Projects */}
+          <MenuItem setActive={setActive} active={active} item="Projects">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="#projects">My Projects</HoveredLink>
+            </div>
+          </MenuItem>
+          {/* Contact */}
+          <MenuItem setActive={setActive} active={active} item="Contact">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="#contact">Contact</HoveredLink>
+              <HoveredLink href="/resume.pdf">Resume</HoveredLink>
+            </div>
+          </MenuItem>
+        </Menu>
+      </div>
+    </div>
+  );
+}
+
